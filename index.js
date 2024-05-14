f1571f68b0c0ff58f2af06199d64c37caad2b2b4
Hakase <hakase@litdevs.org>
2024-05-14 12:00:03 +0200

    Add nonsense

--- index.js
+++ index.js
@@ -1,15 +1,42 @@
 const express = require('express')
 const app = express()
 const cors = require('cors');
+const {join} = require("node:path");
+const mongoose = require("mongoose");
+const cookieParser = require('cookie-parser');
+
 const port = 4146
+app.set('views', join(__dirname, "public"));
+app.set('view engine', 'ejs');
+require('dotenv').config();
 
-// TODO: NONSENSE MVP CODE REPLACE LATER LOL!
-const games = require('./games.json')
-app.get('/api/games', cors(), (req, res) => {
-    res.header("Content-Type",'application/json');
-    res.send(JSON.stringify(games));
+app.use(cookieParser());
+app.use((req, res, next) => {
+    if (req.cookies.admin === process.env.ADMIN_PASSWORD) req.admin = true;
+    next()
 })
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+app.get('/', (req, res) => {
+    res.render('index', {isAdmin: req.admin});
+});
+
+app.get('/create', (req, res) => {
+    if(!req.admin) res.redirect("/");
+    res.render('gamecreate');
+});
+
+app.get('/search', (req, res) => {
+    res.render('search');
+});
+
+app.get('/game/:id', (req, res) => {
+    res.render('game');
+});
+
+async function startApp() {
+    await mongoose.connect(process.env.MONGODB_URL);
+    app.listen(port, () => {
+        console.log(`gamePLUS listening on port ${port}`)
+    })
+}
+startApp();
