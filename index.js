c878dea9cbb4ce2112adfe3891df4f1704abc784
Hakase <hakase@litdevs.org>
2024-05-15 12:02:12 +0200

    game saving and searching

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ const cors = require('cors');
 const {join} = require("node:path");
 const mongoose = require("mongoose");
 const cookieParser = require('cookie-parser');
+const multiparty = require('multiparty');
+const fs = require('fs');
+const {Schema} = require("mongoose");
 
 const port = 4146
+app.use(express.static('public/resources'));
 app.set('views', join(__dirname, "public"));
 app.set('view engine', 'ejs');
 require('dotenv').config();
@@ -25,12 +29,55 @@ app.get('/create', (req, res) => {
     res.render('gamecreate');
 });
 
-app.get('/search', (req, res) => {
-    res.render('search');
+const GameSchema = new Schema({
+    name: String,
+    icon: Buffer,
+    executables: Array
+});
+GameSchema.index({name: 'text'});
+const Game = mongoose.model('Game', GameSchema);
+
+app.post('/create', (req, res) => {
+    if(!req.admin) res.sendStatus(999);
+
+    new multiparty.Form().parse(req, async function(err, fields, files) {
+        const game = new Game();
+        game.name = fields.gamename[0];
+        game.icon = fs.readFileSync(files.icon[0].path);
+        game.executables = []
+
+        Object.keys(fields).forEach(entry => {
+            if(entry.startsWith("executable_platform_")) {
+                let executableIndex = entry.split("executable_platform_")[1];
+
+                game.executables.push({
+                    name: fields[`executable_name_${executableIndex}`][0],
+                    arguments: fields[`executable_arguments_${executableIndex}`][0],
+                    platform: fields[`executable_platform_${executableIndex}`][0],
+                })
+            }
+        })
+
+        await game.save();
+        res.status(200).send("Yay, new yummy game! :D");
+    });
 });
 
-app.get('/game/:id', (req, res) => {
+app.get('/search', async (req, res) => {
+    const find = await Game.find({$text: {$search: req.query.game}}).exec();
+    res.render('search', {find});
+});
+
+app.get('/game/:id', async (req, res) => {
     res.render('game');
+});
+
+app.get('/game/:id/icon', async (req, res) => {
+    const game = await Game.findById(req.params.id).exec();
+
+    res.contentType('image/png');
+    res.set("Content-Disposition", "inline;");
+    res.status(200).send(game.icon);
 });
 
 async function startApp() {
