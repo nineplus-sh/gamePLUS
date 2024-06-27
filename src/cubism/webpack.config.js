966fb93224246c47f8a263ec8ef528ce2a0c6474
Hakase <hakase@litdevs.org>
2024-06-27 22:53:20 +0200

    ignore nonsense error

--- src/cubism/webpack.config.js
+++ src/cubism/webpack.config.js
@@ -7,8 +7,11 @@ module.exports = {
         rules: [
             {
                 test: /\.tsx?$/,
-                use: 'ts-loader',
+                loader: 'ts-loader',
                 exclude: /node_modules/,
+                options: {
+                    ignoreDiagnostics: [2345]
+                }
             },
         ],
     },
