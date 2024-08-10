a384e841d69d638afc4efea7362301a78c16fb92
Hakase <hakase@litdevs.org>
2024-08-10 21:56:12 +0200

    forgot cors

--- index.js
+++ index.js
@@ -257,7 +257,7 @@ const suggestLimit = rateLimit({
     standardHeaders: true,
     legacyHeaders: false,
 })
-app.post('/api/suggest', suggestLimit, async (req, res) => {
+app.post('/api/suggest', cors(), suggestLimit, async (req, res) => {
     let suggestion = new Suggestion();
     suggestion.executable = req.body.executable;
     suggestion.arguments = req.body.arguments;
